b04b754f38c5c6bc443c233c3d27505741529979
yuanmeng <yuanmeng@gmail.com>
2018-12-07 17:37:30 +0800

    1

--- web.go
+++ web.go
@@ -11,24 +11,30 @@ type tomlConfig struct {
 	Redis redis
 }
 type database struct {
-	Server  string
-	Port    int
-	ConnMax int `toml:"connection_max"`
+	Server   string
+	User     string
+	Password string
+	Dbname   string `toml:"db_name"`
+	Port     int
+	ConnMax  int `toml:"connection_max"`
 }
 type redis struct {
 	Server  string
 	Port    int
-	ConnMax int
+	ConnMax int `toml:"connection_max"`
 }
 
 func main() {
-	configBlob, _ := ioutil.ReadFile(".config")
-	config
-	db, err := sql.Open("mysql", "user:password@localhost/mydb")
+	_, err := toml.DecodeFile("config.toml", &tomlConfig)
+	db, err := sql.Open("mysql", tomlConfig.DB.User+":"+tomlConfig.DB.Password+"@"+tomlConfig.DB.Server+"/"+tomlConfig.DB.Dbname)
+	db.SetMaxOpenConns(tomlConfig.DB.ConnMax)
+	db.SetMaxIdleConns(tomlConfig.DB.ConnMax)
+	defer db.Close()
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/post/:id", func(c *gin.Context) {
 		postId := c.Param("id")
+		row, err := 
 	})
 	router.Run(":8080")
 }
