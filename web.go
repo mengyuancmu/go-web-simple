9704aa9fb6ec75955930afdebc8947dcf55b5e74
meng yuan <mengyuan@192.168.0.102>
2018-12-11 21:59:21 +0800

    test

--- web.go
+++ web.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 import "github.com/gin-gonic/gin"
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
@@ -15,26 +17,67 @@ type database struct {
 	User     string
 	Password string
 	Dbname   string `toml:"db_name"`
-	Port     int
-	ConnMax  int `toml:"connection_max"`
+	Port     string `toml:"port"`
+	ConnMax  int    `toml:"connection_max"`
 }
 type redis struct {
 	Server  string
 	Port    int
 	ConnMax int `toml:"connection_max"`
 }
+type post struct {
+	Id      int
+	Content string
+}
+type comment struct {
+	Id      int
+	Content string
+}
 
 func main() {
-	_, err := toml.DecodeFile("config.toml", &tomlConfig)
-	db, err := sql.Open("mysql", tomlConfig.DB.User+":"+tomlConfig.DB.Password+"@"+tomlConfig.DB.Server+"/"+tomlConfig.DB.Dbname)
-	db.SetMaxOpenConns(tomlConfig.DB.ConnMax)
-	db.SetMaxIdleConns(tomlConfig.DB.ConnMax)
+	var config tomlConfig
+	_, err := toml.DecodeFile("config.toml", &config)
+	dsn := config.DB.User + ":" + config.DB.Password + "@tcp(" + config.DB.Server + ":" + config.DB.Port + ")/" + config.DB.Dbname
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(config.DB.ConnMax)
+	db.SetMaxIdleConns(config.DB.ConnMax)
 	defer db.Close()
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/post/:id", func(c *gin.Context) {
 		postId := c.Param("id")
-		row, err := 
+		postRs := make(chan *post)
+		go func() {
+			var postObj post
+			row := db.QueryRow("select * from tb_post where id = ?", postId)
+			row.Scan(&postObj)
+			postRs <- &postObj
+		}()
+		commentRs := make(chan *[]*comment)
+		go func() {
+			commentList := make([]*comment, 5)
+			rows, err := db.Query("select * from tb_comment where post_id = ?", postId)
+			if err != nil {
+				panic(err)
+			}
+			defer rows.Close()
+			var commentObj comment
+			for rows.Next() {
+				rows.Scan(&commentObj)
+				commentList = append(commentList, &commentObj)
+			}
+			commentRs <- &commentList
+		}()
+		postInfo := <-postRs
+		commentListInfo := <-commentRs
+		panic(postInfo.Content)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Post":     postInfo,
+			"Comments": commentListInfo,
+		})
 	})
 	router.Run(":8080")
 }
